fe535cdad8508b61fa41ff05a85a059b04807d6a
himanshuaskd <himanshu.mishra_mca18@gla.ac.in>
2019-03-07 11:02:50 +0530

    user and  password is added in application

--- app/src/main/java/com/example/a7march2019/MainActivity.java
+++ app/src/main/java/com/example/a7march2019/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.a7march2019;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
+    EditText username,password;
+    Button button;
+    String s1,s2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=findViewById(R.id.editText);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user username.getText().toString().trim();
+                String pass password.getText().toString().trim());
+                if (user.equals(s1) && ;pass.equals(s2))
+                {
+                    Toast.makeText(MainActivity.this, "user name very good", Toast.LENGTH_SHORT).show();
+                }
+                else   {
+                    Toast.makeText(MainActivity.this, "user name or  password not correct", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -21,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         SharedPreferences sharedPreferences=getSharedPreferences("com.example.a7march2019",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
-        editor.putString("name",editText.getText().toString().trim());
+        editor.putString("username",username.getText().toString().trim());
+        editor.putString("passwords",password.getText().toString().trim());
         editor.putBoolean("bool",true);
         editor.apply();
 
@@ -31,10 +54,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SharedPreferences sharedPreferences=getSharedPreferences("com.example.a7march2019",MODE_PRIVATE);
-                String s1=sharedPreferences.getString("name","user");
+                s1=sharedPreferences.getString("username","pappu");
+                s2=sharedPreferences.getString("password","papa");
                 Boolean rember=sharedPreferences.getBoolean("bool",false);
                 Toast.makeText(this, "bool"+rember, Toast.LENGTH_SHORT).show();
-                editText.setText(s1);
+                username.setText(s1);
+                password.setText(s2);
+
 
     }
 }
