a5b2b8dcfff4db47570cb8496b2d202f86fd3056
himanshuaskd <himanshu.mishra_mca18@gla.ac.in>
2019-03-07 11:26:38 +0530

    second activity added

--- app/src/main/java/com/example/a7march2019/MainActivity.java
+++ app/src/main/java/com/example/a7march2019/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a7march2019;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,14 +25,15 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user username.getText().toString().trim();
-                String pass password.getText().toString().trim());
-                if (user.equals(s1) && ;pass.equals(s2))
+                String user= username.getText().toString().trim();
+                String pass =password.getText().toString().trim();
+                if (user.equals(s1) && pass.equals(s2))
                 {
-                    Toast.makeText(MainActivity.this, "user name very good", Toast.LENGTH_SHORT).show();
+                    Intent intenta=new Intent(MainActivity.this, secondactivity.class);
+                    startActivity(intenta);
                 }
                 else   {
-                    Toast.makeText(MainActivity.this, "user name or  password not correct", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "user name sahi karo bhai", Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -54,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SharedPreferences sharedPreferences=getSharedPreferences("com.example.a7march2019",MODE_PRIVATE);
-                s1=sharedPreferences.getString("username","pappu");
-                s2=sharedPreferences.getString("password","papa");
+                s1=sharedPreferences.getString("username","mishra ji");
+                s2=sharedPreferences.getString("password","papu");
                 Boolean rember=sharedPreferences.getBoolean("bool",false);
                 Toast.makeText(this, "bool"+rember, Toast.LENGTH_SHORT).show();
                 username.setText(s1);
